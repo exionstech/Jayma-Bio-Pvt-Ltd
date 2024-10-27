00995e2e25a9a1bc2e53f72cb69d70837914b210
Suman Mondal <mondalsumanrock20@gmail.com>
2024-10-27 18:46:40 +0530

    Refactor next.config.js to disable reactStrictMode and update image domains

--- next.config.js
+++ next.config.js
@@ -1,18 +1,12 @@
-const withMDX = require('@next/mdx')({ extension: /\.mdx?$/ });
+const withMDX = require('@next/mdx')()
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: [
-      "images.unsplash.com",
-      "avatars.githubusercontent.com",
-      "img.clerk.com",
-      "res.cloudinary.com",
-      "lh3.googleusercontent.com"
-    ]
+    domains: ["images.unsplash.com", "avatars.githubusercontent.com", "img.clerk.com", "res.cloudinary.com",'lh3.googleusercontent.com']
   },
-  reactStrictMode: true,
+  reactStrictMode: false,
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
-};
+}
 
-module.exports = withMDX(nextConfig);
+module.exports = withMDX(nextConfig)
