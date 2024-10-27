d2ef8f9de0b89f5c263f925a9399976bcf9b2b7f
Suman Mondal <mondalsumanrock20@gmail.com>
2024-10-27 17:52:56 +0530

    feat: Enable MDX runtime and set reactStrictMode to true in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,21 @@
-const withMDX = require('@next/mdx')()
+const withMDX = require('@next/mdx')({ extension: /\.mdx?$/ });
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["images.unsplash.com", "avatars.githubusercontent.com", "img.clerk.com", "res.cloudinary.com",'lh3.googleusercontent.com']
+    domains: [
+      "images.unsplash.com",
+      "avatars.githubusercontent.com",
+      "img.clerk.com",
+      "res.cloudinary.com",
+      "lh3.googleusercontent.com"
+    ]
   },
-  reactStrictMode: false,
+  reactStrictMode: true,
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
-}
+  experimental: {
+    mdxRs: true, // Enables the MDX runtime for better compatibility
+  }
+};
 
-module.exports = withMDX(nextConfig)
+module.exports = withMDX(nextConfig);
