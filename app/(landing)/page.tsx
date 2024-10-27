d4eb1e42ba17ae7d0bc634cc90f82ca8554ee19e
gourab8389 <deyg6988@gmail.com>
2024-10-27 23:46:48 +0530

    Fix import path for MissionSection and add LatestProducts component

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -2,7 +2,7 @@ import { HeroSection } from "@/app/_components/landing/hero";
 import { MaxWrapper } from "@/components/shared/max-wrapper";
 import { Metadata } from "next";
 import Services from "../_components/landing/services";
-import MissionSection from "../_components/landing/misson";
+import MissionSection from "../_components/landing/our-misson";
 
 
 export const metadata: Metadata = {
---
+++ app/_components/landing/latest-products.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LatestProducts = () => {
+  return (
+    <div className='w-full px-5 md:px-14 min-h-screen flex flex-col items-center justify-center max-w-screen-2xl mx-auto h-full pt-10 md:pt-6'>
+      
+    </div>
+  )
+}
+
+export default LatestProducts;
