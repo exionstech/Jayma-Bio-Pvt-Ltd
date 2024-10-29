af55eff2b706fd8f82e1dbb445e73fcf8cb8fb52
Suman Mondal <mondalsumanrock20@gmail.com>
2024-10-29 20:01:08 +0530

    Refactor image grid component and add product section

--- constants/products/products.ts
+++ constants/products/products.ts
@@ -1,23 +1,36 @@
 export const Products = [
   {
-    title: "Product 1",
-    description: "Product 1 description",
-    image: ["/products/pr2.png", "/products/pr1.jpg"],
+    title: "BrewBucha",
+    description:
+      "BrewBucha is a fermented tea loved for its tangy flavor and health benefits. Rich in probiotics and antioxidants, it supports digestion and boosts wellness. An ancient drink, BrewBucha brings natural health to the modern world.",
+    image: [
+      "/products/pr5.png",
+      "/products/pr4.png",
+      "/products/pr6.png",
+      "/products/pr2.png",
+    ],
     price: "100",
     link: "/",
   },
   {
-    title: "Product 2",
-    description: "Product 2 description",
+    title: "Sap Symphony",
+    description:
+      "We create eco-friendly technology that transforms plant signals into music and data, making plant monitoring accessible for everyone. Our solutions foster a deeper connection with nature while supporting sustainable practices.",
     image: ["/products/pr2.png"],
     price: "200",
     link: "/",
   },
   {
-    title: "Product 3",
-    description: "Product 3 description",
-    image: ["/products/pr3.png"],
-    price: "300",
+    title: "BrewBucha",
+    description:
+      "BrewBucha is a fermented tea loved for its tangy flavor and health benefits. Rich in probiotics and antioxidants, it supports digestion and boosts wellness. An ancient drink, BrewBucha brings natural health to the modern world.",
+    image: [
+      "/products/pr5.png",
+      "/products/pr4.png",
+      "/products/pr6.png",
+      "/products/pr2.png",
+    ],
+    price: "100",
     link: "/",
   },
 ];
