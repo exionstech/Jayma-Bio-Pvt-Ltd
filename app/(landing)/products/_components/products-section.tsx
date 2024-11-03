977f0cd6c80e148a27a46fad78a42e760984d185
Suman Mondal <mondalsumanrock20@gmail.com>
2024-11-03 21:53:53 +0530

    feat: enhance wishlist functionality by redirecting to products page when empty and adjust layout spacing

--- app/(landing)/products/_components/products-section.tsx
+++ app/(landing)/products/_components/products-section.tsx
@@ -18,7 +18,7 @@ const ProductsSection = ({ categories, products }: ProductsProps) => {
       className="w-full flex flex-col gap-4 min-h-screen mt-8 md:mt-12 px-5 md:px-10 lg:px-12 max-w-screen-2xl mx-auto"
     >
       <div className="w-full flex items-center justify-center md:justify-end pt-5">
-        <div className="flex items-center gap-3 md:gap-5">
+        <div className="flex items-center gap-2 md:gap-5">
           <div className="flex items-center justify-center">
             <CategotyFilter categories={categories} />
           </div>
