b4fb01e6495084e8360df2edc38c6210cfa8f916
Suman Mondal <mondalsumanrock20@gmail.com>
2024-11-05 02:17:30 +0530

    feat: enhance checkout success page with order confirmation and continue shopping button

--- app/(landing)/checkout-success/page.tsx
+++ app/(landing)/checkout-success/page.tsx
@@ -1,13 +1,23 @@
 "use client";
 
 import { getUrl } from "@/actions/get-url";
+import { MaxWrapper } from "@/components/shared/max-wrapper";
+import { Button } from "@/components/ui/button";
 import useCart from "@/hooks/products/use-carts";
 import axios from "axios";
+import { ChevronLeft } from "lucide-react";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 import React, { useEffect } from "react";
 
 const Page = () => {
   const params = new URLSearchParams(window.location.search);
   const orderId = params.get("order_id");
+  const router = useRouter();
+
+  if (!orderId) {
+    router.replace("/products");
+  }
 
   const cart = useCart();
 
@@ -41,7 +51,35 @@ const Page = () => {
     handleWebhook();
   }, []);
 
-  return <div className="mt-40">Success</div>;
+  return (
+    <MaxWrapper>
+      <div className="w-full flex pt-5 md:pt-20 h-full min-h-[70vh] md:min-h-[80vh] items-center justify-center">
+        <div className="w-full flex flex-col gap-5 md:gap-8 pt-10 md:pt-0">
+          <div className="w-full flex flex-col items-center justify-center gap-6">
+            <h1 className="text-2xl md:text-4xl font-medium text-center text-green">
+              Your order has been placed successfully !
+            </h1>
+            <img
+              src="/cart/success.svg"
+              alt="cart"
+              className="w-44 md:w-72 select-none mt-5 md:mt-8"
+            />
+          </div>
+          <div className="w-fill flex items-center justify-center mt-7">
+            <Link href="/products">
+              <Button
+                className="w-[240px] md:w-[280px] flex items-center gap-2 border-none"
+                variant="ghost"
+              >
+                <ChevronLeft className="size-6 shrink-0 text-green" />
+                Continue Shopping
+              </Button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </MaxWrapper>
+  );
 };
 
 export default Page;
