e0b9b14615d2cbcac2082f41e4d3452dc90fbf6f
Suman Mondal <mondalsumanrock20@gmail.com>
2024-11-08 05:03:37 +0530

    feat: streamline webhook handling by removing redundant checks and improving flow

--- app/progress/page.tsx
+++ app/progress/page.tsx
@@ -1,67 +1,3 @@
-// "use client";
-
-// import { getUrl } from "@/actions/get-url";
-// import Loader from "@/components/shared/loader";
-// import axios from "axios";
-// import { useRouter } from "next/navigation";
-// import React, { useEffect } from "react";
-
-// const Page = () => {
-//   const params = new URLSearchParams(window.location.search);
-//   const orderId = params.get("order_id");
-
-//   const router = useRouter();
-
-//   useEffect(() => {
-//     if (!orderId) {
-//       return;
-//     }
-//   }, [orderId]);
-
-//   const handleWebhook = async () => {
-//     const URL = await getUrl().then((data) => {
-//       if (data.data) {
-//         return `${data.data.storeId}`;
-//       }
-//     });
-
-//     const response = await axios.post(
-//       `${process.env.NEXT_PUBLIC_WEBHOOK_STORE_URL}/${URL}/status`,
-//       {
-//         orderId: orderId,
-//       }
-//     );
-
-//     if (response.data.status === 200) {
-//       console.log(response.data.data);
-
-//       if (response.data.data.payment_status === "SUCCESS") {
-//         router.push(
-//           "/checkout-success?order_id=" +
-//             orderId +
-//             "&payment_id=" +
-//             response.data.data.cf_payment_id +
-//             "&status=" +
-//             response.data.data.payment_status.toLowerCase()
-//         );
-//       } else if (response.data.data.payment_status === "FAILURE") {
-//         router.push("/checkout-failed");
-//       } else {
-//         router.push("/checkout-cancelled");
-//       }
-//       localStorage.removeItem("url");
-//     }
-//   };
-
-//   useEffect(() => {
-//     handleWebhook();
-//   }, []);
-
-//   return <Loader />;
-// };
-
-// export default Page;
-
 "use client";
 
 import { getUrl } from "@/actions/get-url";
@@ -98,12 +34,6 @@ const Page = () => {
 
     if (response.data.status === 200) {
       console.log(response.data.data);
-
-      if (!response.data.data.cf_payment_id) {
-        router.push("/checkout-cancelled");
-        localStorage.removeItem("url");
-        return;
-      }
 
       if (response.data.data.payment_status === "SUCCESS") {
         router.push(
