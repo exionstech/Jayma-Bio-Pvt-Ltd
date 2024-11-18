0dac1c34f6559385e5cf6eefbb933b01dc35bad1
Suman Mondal <mondalsumanrock20@gmail.com>
2024-11-18 16:25:35 +0530

    feat: Add CategoryDetails component and integrate it into the category page

--- app/(landing)/categories/[categoryId]/page.tsx
+++ app/(landing)/categories/[categoryId]/page.tsx
@@ -1,13 +1,18 @@
 import getCategory from "@/actions/products/get-category";
 import getProducts from "@/actions/products/get-products";
+import CategoryDetails from "./_components/category-details";
+import { Metadata } from "next";
 
 interface CategoryDetailsProps {
   params: {
     categoryId: string;
   };
 }
+export const metadata: Metadata = {
+  title: "Product | Jayma Bio Innovations",
+};
 
-const CategoryDetails = async ({ params }: CategoryDetailsProps) => {
+const CategoryDetailsPage = async ({ params }: CategoryDetailsProps) => {
   const category = await getCategory(params.categoryId);
   const products = await getProducts({
     category: category.name,
@@ -15,18 +20,11 @@ const CategoryDetails = async ({ params }: CategoryDetailsProps) => {
   return (
     <section
       id="category"
-      className="w-full min-h-screen py-8 md:py-12 mt-4 md:mt-8"
+      className="w-full min-h-screen py-6 md:py-12 mt-4 md:mt-6 px-5 md:px-10 lg:px-12 max-w-screen-2xl mx-auto"
     >
-      <div className="max-w-screen-2xl mx-auto px-5 md:px-8 lg:px-12 w-full flex flex-col gap-6 md:gap-8">
-        {category.name}
-      </div>
-      {products.map((product) => (
-        <div>
-            {product.name}
-        </div>
-      ))}
+      <CategoryDetails category={category} products={products} />
     </section>
   );
 };
 
-export default CategoryDetails;
+export default CategoryDetailsPage;
--- app/(landing)/categories/_components/categories-page-detail.tsx
+++ app/(landing)/categories/_components/categories-page-detail.tsx
@@ -1,48 +1,68 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
-import { Card, CardDescription, CardTitle } from "@/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardTitle,
+} from "@/components/ui/card";
 import { Category } from "@/types/products-related-types";
-import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 interface CategoriesPageDetailsProps {
   categories: Category[];
 }
 const CategoriesPageDetails = ({ categories }: CategoriesPageDetailsProps) => {
+  const router = useRouter();
+  const [redirecting, setRedirecting] = useState(false);
   return (
     <section
       id="categories"
       className="w-full min-h-screen py-8 md:py-12 mt-4 md:mt-10"
     >
       <div className="max-w-screen-2xl mx-auto px-5 md:px-8 lg:px-12 w-full flex flex-col gap-6 md:gap-10">
-        <div className="flex flex-col items-center gap-6 md:gap-12 mb-8 pt-4 md:pt-0">
+        <div className="flex flex-col items-center gap-6 md:gap-12 mb-4 md:mb-8 pt-4 md:pt-0">
           <h1 className="text-4xl md:text-7xl font-semibold text-green select-none">
             All products
           </h1>
         </div>
-        <div className="w-full grid grid-cols-9 gap-8">
+        <div className="w-full grid grid-cols-3 md:grid-cols-9 gap-8">
           {categories.map((category) => (
-            <Card
-              key={category.id}
-              className="col-span-3 flex flex-col gap-4 px-3 py-4 shadow-md z-20"
-            >
-              <div className="object-contain rounded-xl overflow-hidden">
-                <img
-                  src={category.banner}
-                  alt="banner"
-                  className="w-full h-auto object-cover"
-                />
-              </div>
-              <div className="flex flex-col gap-2">
-                <CardTitle className="text-lg font-semibold">
-                  {category.name}
-                </CardTitle>
-                <CardDescription>{category.description}</CardDescription>
-              </div>
-              <div className="w-full flex items-center justify-end">
-                <Link href={`/categories/${category.id}`}>
-                  <Button className="rounded-xl" size={"sm"}>
-                    View Products
-                  </Button>
-                </Link>
+            <Card className="overflow-hidden bg-white rounded-lg shadow-md col-span-3">
+              <div className="relative p-2">
+                <div className="aspect-square relative rounded-lg border overflow-hidden">
+                  <img
+                    src={category.banner}
+                    alt={category.name}
+                    className="absolute inset-0 w-full h-full object-cover rounded-lg"
+                  />
+                </div>
+
+                <CardContent className="p-4 pb-6">
+                  <div className="w-full flex flex-col gap-1 min-h-[8rem]">
+                    <h3 className="text-lg font-semibold text-green mb-2">
+                      {category.name}
+                    </h3>
+                    <p className="text-sm text-green mb-4 line-clamp-2">
+                      {category.description}
+                    </p>
+                  </div>
+                  <div className="flex items-center justify-between">
+                    <Button
+                      variant="default"
+                      disabled={redirecting}
+                      className="bg-green text-white hover:bg-green/90 px-4 rounded-xl"
+                      onClick={() => (
+                        setRedirecting(true),
+                        router.push(`/categories/${category.id}`)
+                      )}
+                    >
+                      View Details
+                    </Button>
+                  </div>
+                </CardContent>
               </div>
             </Card>
           ))}
